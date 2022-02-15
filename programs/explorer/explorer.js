db3660b5094604dae04a7c4799d2b4a120f538e5
LNV <99676956+bruhLNV@users.noreply.github.com>
2022-02-15 10:10:38 +0100

    Update explorer.js

--- programs/explorer/explorer.js
+++ programs/explorer/explorer.js
@@ -961,7 +961,7 @@ function can_go_up() {
 }
 
 function go_home() {
-	go_to("https://isaiahodhner.io/"); // My personal homepage; I might use a search engine, but they don't support iframes
+	go_to("https://github.com/bruhLNV/"); // My GitHub
 }
 
 function executeFile(file_path) {
